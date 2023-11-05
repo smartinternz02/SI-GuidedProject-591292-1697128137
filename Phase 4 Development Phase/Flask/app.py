d71df78083ef7b43173c34a471ec16e592de05b5
Anurag Mishra <doitmywayweb@gmail.com>
2023-11-05 17:04:45 +0530

    --

--- Phase 4 Development Phase/Flask/app.py
+++ Phase 4 Development Phase/Flask/app.py
@@ -22,18 +22,35 @@ def home1():
     return render_template("predict.html")
 
 
+# def predict():
+#     x = [[x for x in request.form.values()]]
+#     print(x)
+#
+#     x = np.array(x)
+#     print(x.shape)
+#     print(x)
+#
+#     pred = model.predict(x)
+#     print(pred[0])
+#
+#     return render_template("submit.html", prediction_text=str(pred))
+
+@app.route('/predict', methods=['POST'])
 def predict():
-    x = [[x for x in request.form.values()]]
-    print(x)
 
-    x = np.array(x)
-    print(x.shape)
-    print(x)
+    # Get user input from the form
+    feature1 = float(request.form['step'])
+    feature2 = float(request.form['type'])
+    feature3 = float(request.form['amount'])
+    feature4 = float(request.form['oldbalanceOrg'])
+    feature5 = float(request.form['newbalanceOrg'])
+    feature6 = float(request.form['oldbalanceDest'])
+    feature7 = float(request.form['newbalanceDest'])
 
-    pred = model.predict(x)
-    print(pred[0])
+    # Make a prediction using the loaded model
+    prediction = model.predict([[feature1, feature2, feature3, feature4, feature5, feature6, feature7]])
 
-    return render_template("submit.html", prediction_text=str(pred))
+    return f'The given payment : {prediction[0]}'
 
 
 if __name__ == "__main__":
